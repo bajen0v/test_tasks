df417b82155f043a32c7b0fa6e6f0a3a59bc06d9
bajen0v <ibajenov1991@gmail.com>
2023-10-07 15:11:29 +0400

    рефактор задачи переноса элементов списка

--- butterflyControl.js
+++ butterflyControl.js
@@ -10,76 +10,70 @@ let selectedCollection = document.getElementById("selected");
 let activeAvailable = availableCollection.getElementsByTagName("li");
 let activeSelected = selectedCollection.getElementsByTagName("li");
 
-function click(e) {
-  let setContent = [];
-  let length = 0;
+const checkActions = {
+  'available': (e) => e.target.classList.toggle("active"),
+  'selected': (e) => e.target.classList.toggle("active"),
+};
 
-  let check = e.target.parentNode.id;
-  switch (check) {
-    case "available":
-      e.target.classList.toggle("active");
-      break;
-    case "selected":
-      e.target.classList.toggle("active");
-      break;
-
-    default:
-      switch (e.target.id) {
-        case "1":
-          selectedCollection.innerHTML += availableCollection.innerHTML;
-          availableCollection.innerHTML = null;
-          for (let elem of activeSelected) {
-            elem.classList.remove("active");
-          }
-
-          break;
-        case "2":
-          setContent = []; // у каждого зануляется массив ли и длина массива, которые мы хотим переместить
-          length = 0;
-          Array.from(availableCollection.children).map((el) => {
-            el.classList.contains("active")
-              ? ((length += 1),
-                el.classList.remove("active"),
-                setContent.push(el.cloneNode(true)),
-                availableCollection.removeChild(el))
-              : availableCollection.appendChild(el);
-          });
-          length > 0
-            ? (setContent.map((el) => selectedCollection.appendChild(el)),
-              console.log("Добавляем"),
-              console.log(setContent))
-            : alert("Ошибка выберерите элемент из списка");
-
-          break;
-        case "3":
-          setContent = [];
-          length = 0;
-          console.log(selectedCollection);
-          Array.from(selectedCollection.children).map((el) => {
-            el.classList.contains("active")
-              ? ((length += 1),
-                el.classList.remove("active"),
-                setContent.push(el.cloneNode(true)),
-                selectedCollection.removeChild(el))
-              : selectedCollection.appendChild(el);
-          });
-          length > 0
-            ? (console.log("Добавляем"),
-              setContent.map((el) => availableCollection.appendChild(el)))
-            : alert("Ошибка выберерите элемент из списка");
-          break;
-        case "4":
-          availableCollection.innerHTML += selectedCollection.innerHTML;
-          selectedCollection.innerHTML = null;
-          for (let elem of activeAvailable) {
-            elem.classList.remove("active");
-          }
-
-          break;
-        default:
-          alert("не кнопка");
-      }
-  }
+const swapFromAvaibleAll = () => {
+    selectedCollection.innerHTML += availableCollection.innerHTML;
+    availableCollection.innerHTML = null;
+    for (let elem of activeSelected) {
+      elem.classList.remove("active");
+    }
 }
 
-container.onclick = click;
+const swapFromAvaibleSingle = () => {
+    setContent = []; // у каждого зануляется массив ли и длина массива, которые мы хотим переместить
+    length = 0;
+    Array.from(availableCollection.children).map((el) => {
+      el.classList.contains("active")
+        ? ((length += 1),
+          el.classList.remove("active"),
+          setContent.push(el.cloneNode(true)),
+          availableCollection.removeChild(el))
+        : availableCollection.appendChild(el);
+    });
+    length > 0 ? setContent.map((el) => selectedCollection.appendChild(el)) : alert("Ошибка выберерите элемент из списка");
+}
+
+const swapFromSelectedSingle = () => {
+    setContent = [];
+    length = 0;
+    Array.from(selectedCollection.children).map((el) => {
+      el.classList.contains("active")
+        ? ((length += 1),
+          el.classList.remove("active"),
+          setContent.push(el.cloneNode(true)),
+          selectedCollection.removeChild(el))
+        : selectedCollection.appendChild(el);
+    });
+    length > 0 ? setContent.map((el) => availableCollection.appendChild(el)) : alert("Ошибка выберерите элемент из списка");
+}
+
+const swapFromSelectedAll = () => {
+    availableCollection.innerHTML += selectedCollection.innerHTML;
+    selectedCollection.innerHTML = null;
+    for (let elem of activeAvailable) {
+      elem.classList.remove("active");
+    }
+}
+
+const checkClickBtn = {
+    '1': swapFromAvaibleAll,
+    '2': swapFromAvaibleSingle,
+    '3': swapFromSelectedSingle,
+    '4': swapFromSelectedAll
+}
+
+function checkedEvent(e) {
+  let setContent,length
+
+  let check = e.target.parentNode.id;
+  let clickBtn = e.target.id
+  return check ? checkActions[check](e) : checkClickBtn[clickBtn](e)
+
+}
+
+
+container.onclick = checkedEvent;
